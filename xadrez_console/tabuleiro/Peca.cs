e65101e3a0c5ee158ebac0c72c663f1610e8d10e
Matheus Rodrigues <mmrodrigues077@gmail.com>
2023-06-28 16:01:48 -0300

    Implementando peças no tabuleiro

--- xadrez_console/tabuleiro/Peca.cs
+++ xadrez_console/tabuleiro/Peca.cs
@@ -9,9 +9,9 @@ namespace tabuleiro
         public int qteMovimentos { get; set; }
         public Tabuleiro tab { get; protected set; }
 
-        public Peca (Posicao posicao, Tabuleiro tabuleiro, Cor cor)
+        public Peca (Tabuleiro tabuleiro, Cor cor)
         {
-            this.posicao = posicao;
+            this.posicao = null;
             this.tab = tab;
             this.cor = cor;
             this.qteMovimentos = 0;
