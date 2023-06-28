573b1b334d5f30950ecb1b3f20e8897c13847e32
Matheus Rodrigues <mmrodrigues077@gmail.com>
2023-06-28 16:30:42 -0300

    Tratando erros com exceção personalida

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -8,14 +8,20 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            tab.colocarPeca(new Torre(tab, Cor.Amarela), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Amarela), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Amarela), new Posicao(2, 4));
+                tab.colocarPeca(new Torre(tab, Cor.Amarela), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Amarela), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Amarela), new Posicao(7, 7));
 
-            Tela.imprimirTabuleiro(tab);
-
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
         }
     }
